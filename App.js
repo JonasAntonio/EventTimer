2185469490e18756505cfcc685ac7b03b1ee50cc
JonasAntonio <jayv.jonas@gmail.com>
2021-02-05 13:10:33 -0300

    início

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Display from './src/components/Display'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  endTime = new Date('2021', '02', '05', '20', '00', '00', '00')
+  
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={'a'}></Display>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
